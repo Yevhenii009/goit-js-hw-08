c8a322537dc985b38c9776f0262af43e0a0f96fe
Yevhenii009 <stifgun@gmail.com>
2022-12-23 11:42:33 +0200

    дз1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galeryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryItemsMarkup =
+    galleryItems.map(({ preview, original, description }) => 
+    `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}"/>
+    </a>
+    `
+    ).join('');
+
+galeryContainer.insertAdjacentHTML('beforeend', createGalleryItemsMarkup);
+    
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
