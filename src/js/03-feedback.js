1319b77e949fb04b839356db0ad1aaed7514e5e8
Yevhenii009 <stifgun@gmail.com>
2022-12-24 21:58:37 +0200

    дз3-7

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -34,7 +34,7 @@ function formValues() {
   }
 }
 
-function onAddFormInput(event) {
+function onAddForm(event) {
   formData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
@@ -47,57 +47,3 @@ function onAddForm(event) {
   formData = {};
 }
 
-
-
-
-
-
-
-// import throttle from 'lodash.throttle';
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const form = document.querySelector('.feedback-form');
-// const email = document.querySelector('.feedback-form input');
-// const message = document.querySelector('.feedback-form textarea');
-
-// form.addEventListener('submit', onFormSubmit);
-// form.addEventListener('input', throttle(onFormInput, 500));
-
-// let formData = {};
-
-// fillingPreviousValues();
-
-// function fillingPreviousValues() {
-//   let savedMessage = localStorage.getItem(STORAGE_KEY);
-
-//   try {
-//     savedMessage = JSON.parse(savedMessage);
-//   } catch (error) {
-//     console.log(error.name);
-//     console.log(error.message);
-//   }
-
-//   if (savedMessage) {
-//     if (savedMessage.email) {
-//       email.value = savedMessage.email;
-//       formData.email = savedMessage.email;
-//     }
-//     if (savedMessage.message) {
-//       message.value = savedMessage.message;
-//       formData.message = savedMessage.message;
-//     }
-//   }
-// }
-
-// function onFormInput(evt) {
-//   formData[evt.target.name] = evt.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function onFormSubmit(evt) {
-//   evt.preventDefault();
-//   console.log(formData);
-//   evt.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-//   formData = {};
-// }
