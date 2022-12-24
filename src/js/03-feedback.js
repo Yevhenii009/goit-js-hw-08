55b35671e0c9d5623a8924fcc72e83dd6e167c4e
Yevhenii009 <stifgun@gmail.com>
2022-12-24 20:52:53 +0200

    дз3-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -41,7 +41,7 @@ function onAddFormInput(event) {
 
 function onAddForm(event) {
   event.preventDefault();
-  // console.log(formData);
+  console.log(formData);
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
   formData = [];
